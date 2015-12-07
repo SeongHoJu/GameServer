fe625c7417fa587f5cf9d9106786df063132c28a
SeongHoJu <hakgu87@gmail.com>
2015-12-07 19:15:02 +0900

    ASP.NET MVC5 Tutorial 1 - Getting Started with ASP.NET MVC5
    ASP.NET MVC5 Tutorial 1 - Getting Started with ASP.NET MVC5

---
+++ GameServer/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameServer.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
