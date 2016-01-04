51aee47485cb1cd2c99373108032f5c7e28bd0fa
SeongHoJu <hakgu87@gmail.com>
2016-01-04 17:52:23 +0900

    [PROG_ADD] ASP.NET MVC5 Tutorial 3 - Adding View

--- GameServer/Controllers/HelloWorldController.cs
+++ GameServer/Controllers/HelloWorldController.cs
@@ -16,9 +16,12 @@ namespace GameServer.Controllers
 
         // 
         // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
