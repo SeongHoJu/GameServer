f0b49d4442689db975ac662818ae9b7ce6beacbb
SeongHoJu <hakgu87@gmail.com>
2016-01-14 18:13:34 +0900

    [PROG_ADD] ASP.NET MVC5 Tutorial 4 - Adding Model
    [PROG_ADD] ASP.NET MVC5 Tutorial 4 - Adding Model

---
+++ GameServer/Models/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+
+namespace GameServer.Models
+{
+    public class UserInfo
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public DateTime LastestConnectDate { get; set; }
+    }
+
+    public class UserInfoDBContext : DbContext
+    {
+        public DbSet<UserInfo> UserLs { get; set; }
+    }
+}
